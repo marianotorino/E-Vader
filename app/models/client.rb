c2570cb5367aeeb3fc44a377363aba2802add22a
ramiro <ramiro.antonio@outlook.com>
2015-12-18 13:37:21 -0300

    Corregidas algunas validaciones con expresiones regulares

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,8 +2,9 @@ class Client < ActiveRecord::Base
   has_many :contacts, dependent: :destroy
   has_many :bills, dependent: :destroy
   validates :name, :surname, :cuit, :dni, :gender, :birthdate, presence: true
-  validates :name, :surname, format: { with: /[a-zA-Z\s]/,
+  validates :name, :surname, format: { with: /\A[a-zA-Z\s]+\z/,
     message: "sólo permite letras" }
+  validates :cuit, format: { with: /\A\d{2}-{1}\d{8}-{1}\d{1}\z/, message: "debe tener el formato XX-XXXXXXXX-X" }
   validates :gender, inclusion: { in: %w(M F), message: "admite los valores M o F" }
   validates :dni, numericality: { only_integers: true }
   validates :dni, length: { minimum: 7, maximum: 9 }
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
   validates :name, :cuit, presence: true
   validates :cuit, uniqueness: { message: "ya existe en el sistema" }
+  validates :cuit, format: { with: /\A\d{2}-{1}\d{8}-{1}\d{1}\z/, message: "debe tener el formato XX-XXXXXXXX-X" }
 end
